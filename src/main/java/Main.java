2a2f4c495b28a7b62c19af3171ac838201b11141
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2021-05-11 17:51:11 +0200

    Import libs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello world");
+
     }
 }
